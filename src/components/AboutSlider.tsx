3235dc7e3e8e2fdab9d21c813a8b00177d75eab2
shobhitsangwan13 <shobhitsangwan13@gmail.com>
2025-04-01 15:52:38 +0530

    responsive design

--- src/components/AboutSlider.tsx
+++ src/components/AboutSlider.tsx
@@ -16,40 +16,76 @@ interface AboutSlide {
 }
 
 export default function AboutSlider({ slides }: { slides: AboutSlide[] }) {
+  // const getColoredHeading = (mainHeading: string, slideHeading: string, slideIndex: number) => {
+  //   const words = mainHeading.split(' ');
+  //   const altruistIndex = words.findIndex(word => 
+  //     word.toLowerCase() === 'altruist'
+  //   );
+
+  //   if (altruistIndex !== -1) {
+  //     const altruist = words[altruistIndex];
+  //     const firstCharSlideHeading = slideHeading.charAt(0).toLowerCase();
+      
+  //     // Find all positions of the matching character
+  //     const positions: number[] = [];
+  //     altruist.split('').forEach((char, index) => {
+  //       if (char.toLowerCase() === firstCharSlideHeading) {
+  //         positions.push(index);
+  //       }
+  //     });
+
+  //     // Choose which occurrence to color based on slide index
+  //     const occurrenceToColor = slideIndex % positions.length;
+      
+  //     const coloredAltruist = altruist.split('').map((char, index) => {
+  //       if (char.toLowerCase() === firstCharSlideHeading && 
+  //           index === positions[occurrenceToColor]) {
+  //         return `<span style="color: #0066FF;">${char}</span>`;
+  //       }
+  //       return char;
+  //     }).join('');
+      
+  //     words[altruistIndex] = coloredAltruist;
+  //   }
+
+  //   return words.join(' ');
+  // };
   const getColoredHeading = (mainHeading: string, slideHeading: string, slideIndex: number) => {
     const words = mainHeading.split(' ');
-    const altruistIndex = words.findIndex(word => 
-      word.toLowerCase() === 'altruist'
-    );
-
+    const altruistIndex = words.findIndex(word => word.toLowerCase() === 'altruist');
+  
     if (altruistIndex !== -1) {
-      const altruist = words[altruistIndex];
-      const firstCharSlideHeading = slideHeading.charAt(0).toLowerCase();
+      let altruist = words[altruistIndex];
       
-      // Find all positions of the matching character
-      const positions: number[] = [];
-      altruist.split('').forEach((char, index) => {
-        if (char.toLowerCase() === firstCharSlideHeading) {
-          positions.push(index);
-        }
-      });
-
-      // Choose which occurrence to color based on slide index
-      const occurrenceToColor = slideIndex % positions.length;
+      // Choose the character to color based on the slideIndex
+      const characterToColor = slideIndex % altruist.length; // This will cycle through the characters of 'altruist'
+  
+      // Split the word into parts
+      const beforeChar = altruist.slice(0, characterToColor);
+      const charToColor = altruist.charAt(characterToColor);
+      const afterChar = altruist.slice(characterToColor + 1);
+  
+      // Create the colored word with the selected character
+      const coloredAltruist = `
+        <span>${beforeChar}</span><span style="color: #0066FF;">${charToColor}</span><span>${afterChar}</span>
+      `;
       
-      const coloredAltruist = altruist.split('').map((char, index) => {
-        if (char.toLowerCase() === firstCharSlideHeading && 
-            index === positions[occurrenceToColor]) {
-          return `<span style="color: #0066FF;">${char}</span>`;
-        }
-        return char;
-      }).join('');
-      
+      // Update the words array with the colored "altruist" word
       words[altruistIndex] = coloredAltruist;
     }
-
-    return words.join(' ');
+  
+    // Create the final result where the whole "altruist" word is wrapped in a <span> and selected character is colored
+    return words.map((word, i) => {
+      if (i === altruistIndex) {
+        // If it's the "altruist" word, we already handled the color, so return it directly
+        return word;
+      } else {
+        // Wrap other words in <span>
+        return `<span>${word}</span>`;
+      }
+    }).join(' ');
   };
+  
 
   return (
     <section id="who-we-are" className="about-section py- px-0 pb-0">
