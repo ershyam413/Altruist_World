43ae1d108fd25d04ab7708ba6bb3c69ca2a8a58a
ershyam413 <er.shyam413@gmail.com>
2025-04-01 14:05:34 +0530

    contact us

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
